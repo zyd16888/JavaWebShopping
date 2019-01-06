a80adb8e44afc8def5dfae41cd8cf0786657cfad
zyd16888 <zyd475182659@163.com>
2019-01-06 16:15:38 +0800

    :sparkles: 更改数据类型

--- src/com/singlelovely/entity/User.java
+++ src/com/singlelovely/entity/User.java
@@ -29,79 +29,100 @@ public class User {
 	private String telephone;
 	private String birthday;
 	private String sex;
-	private String state;
+	private int state;
 	private String code;
 	
 	public User() {
 		
 	}
-	
+
 	public String getUid() {
 		return uid;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public String getPhoto() {
 		return photo;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public String getTelephone() {
 		return telephone;
 	}
+
 	public String getBirthday() {
 		return birthday;
 	}
+
 	public String getSex() {
 		return sex;
 	}
-	public String getState() {
+
+	public int getState() {
 		return state;
 	}
+
 	public String getCode() {
 		return code;
 	}
+
 	public void setUid(String uid) {
 		this.uid = uid;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public void setPhoto(String photo) {
 		this.photo = photo;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
+
 	public void setBirthday(String birthday) {
 		this.birthday = birthday;
 	}
+
 	public void setSex(String sex) {
 		this.sex = sex;
 	}
-	public void setState(String state) {
+
+	public void setState(int state) {
 		this.state = state;
 	}
+
 	public void setCode(String code) {
 		this.code = code;
 	}
 	
-	
 }
+	
