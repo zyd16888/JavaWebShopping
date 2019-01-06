72b758137c89dc1b904464356e343b6e9b9b180f
zyd16888 <zyd475182659@163.com>
2019-01-06 17:11:01 +0800

    :white_check_mark: UUID测试类

---
+++ src/com/singlelovely/test/UUIDTest.java
@@ -0,0 +1,9 @@
+package com.singlelovely.test;
+
+import java.util.UUID;
+
+public class UUIDTest {
+	public static void main(String[] args) {
+		System.out.println(UUID.randomUUID().toString().replaceAll("-",""));
+	}
+}
