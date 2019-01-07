4bdef80548ffab701c1268a030ecd4e381b790c4
zyd16888 <zyd475182659@163.com>
2019-01-07 15:01:38 +0800

    :sparkles: 增加登陆方法

--- src/com/singlelovely/dao/UserDao.java
+++ src/com/singlelovely/dao/UserDao.java
@@ -16,9 +16,18 @@ public interface UserDao {
 	/**
 	 * 增加用户方法
 	 * @param user  对象中封装了要插入到数据库中的数据
-	 * @param user
-	 * @return
+	 * @return		插入的记录数
 	 */
 	
 	public int saveUser(User user);
+	
+	
+	
+	/**
+	 * 增加用户方法
+	 * @param user  对象中封装了要插入到数据库中的数据
+	 * @param password
+	 * @return
+	 */
+	public User login(String username, String password);
 }
