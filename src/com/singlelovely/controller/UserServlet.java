3ddae1c32f8ecf71ee82c71d3e980ea51043378d
zyd16888 <zyd475182659@163.com>
2019-01-08 19:34:09 +0800

    :arrow_down: 适配tomcat7.0.73

--- src/com/singlelovely/controller/UserServlet.java
+++ src/com/singlelovely/controller/UserServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.eclipse.jdt.internal.compiler.parser.RecoveredRequiresStatement;
+//import org.eclipse.jdt.internal.compiler.parser.RecoveredRequiresStatement;
 
 import com.singlelovely.dao.UserDao;
 import com.singlelovely.dao.impl.UserDaoImpl;
