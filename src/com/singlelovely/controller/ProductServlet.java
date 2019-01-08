193671f6928d10d310bfc9d7f9e78077ae842bfc
zyd16888 <zyd475182659@163.com>
2019-01-08 19:30:01 +0800

    :tada: 产品分类页面Servlet

---
+++ src/com/singlelovely/controller/ProductServlet.java
@@ -0,0 +1,46 @@
+package com.singlelovely.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.singlelovely.dao.ProductDao;
+import com.singlelovely.dao.impl.ProductDaoImpl;
+import com.singlelovely.entity.Product;
+
+
+public class ProductServlet extends HttpServlet {
+	private ProductDao dao = new ProductDaoImpl();
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String method = request.getParameter("method");
+		if (method.equals("findProductListByCid")) {
+			String cid = request.getParameter("cid");
+			findProductListByCid(request,response,cid);
+		}
+		
+	}
+
+	
+	public void findProductListByCid(HttpServletRequest request, HttpServletResponse response, String cid) throws ServletException, IOException {
+		
+		//根据cid查询数据库
+		List<Product> list = dao.findProductListByCid(cid);
+		//将商品信息存放到request域
+		request.setAttribute("productList", list);
+		
+		//跳转到product_list页面
+		request.getRequestDispatcher("product_list.jsp").forward(request, response);;
+		return;
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
