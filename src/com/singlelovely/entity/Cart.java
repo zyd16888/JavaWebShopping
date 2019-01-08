4d996bca8420d5e74d7d6d03066e83758457e32a
zyd16888 <zyd475182659@163.com>
2019-01-09 00:31:16 +0800

    :tada: Cart页面对应的实体类

---
+++ src/com/singlelovely/entity/Cart.java
@@ -0,0 +1,20 @@
+package com.singlelovely.entity;
+
+public class Cart {
+	private String pid;
+	private int count;
+	public String getPid() {
+		return pid;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	
+}
