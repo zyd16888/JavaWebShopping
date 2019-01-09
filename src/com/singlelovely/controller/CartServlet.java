29af4c45d36f94e86e3eb2cc71c7d2c272211b1c
zyd16888 <zyd475182659@163.com>
2019-01-09 15:52:41 +0800

    :hammer: :sparkles: 重构页面展示方法，加入数量修改删除等功能

--- src/com/singlelovely/controller/CartServlet.java
+++ src/com/singlelovely/controller/CartServlet.java
@@ -1,5 +1,6 @@
 package com.singlelovely.controller;
 
+import java.awt.event.ItemEvent;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpSession;
 import com.singlelovely.dao.ProductDao;
 import com.singlelovely.dao.impl.ProductDaoImpl;
 import com.singlelovely.entity.Cart;
+import com.singlelovely.entity.CartItem;
 import com.singlelovely.entity.Product;
 
 /**
@@ -27,58 +29,91 @@ public class CartServlet extends HttpServlet {
 		String method = request.getParameter("method");
 		if (method.equals("addCart")) {
 			addCart(request, response);
+		}else if (method.equals("changeNumber")) {
+			changeNumber(request,response);
+		}else if (method.equals("deleteCart")) {
+			deleteCart(request,response);
+		}else if (method.equals("clearCart")) {
+			clearCart(request,response);
 		}
 		
 	}
 	
 	
-	public void addCart(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private void clearCart(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		HttpSession session = request.getSession();
-		int count = Integer.parseInt(request.getParameter("count"));//数量
-		String pid = request.getParameter("pid");//商品id
+		Cart cart = (Cart) session.getAttribute("cart");
+		//清除购物车信息
+		cart.getCart().clear();
+		response.sendRedirect("cart.jsp");
 		
-		Cart c = new Cart();
-		c.setCount(count);
-		c.setPid(pid);
-		//从session中获取已有的购物车对象集合
-		List<Cart> cartList = (List<Cart>) session.getAttribute("cartList");
-		
-		if (cartList == null) {
-			cartList = new ArrayList<Cart>();
-		}
-		
-		//购物车中商品的集合
-		//List<Cart> list = new ArrayList<Cart>();		
-		
-		
-		
-		cartList.add(c);
-		
-		
-		session.setAttribute("cartList", cartList);
-		
-		//添加成功后跳转到购物车页面
-		showCart(request,response);
 	}
-	
-	//显示购物车页面
-	public void showCart(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+	private void deleteCart(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String pid = request.getParameter("pid");
+		//获取购物车对象
 		HttpSession session = request.getSession();
-		//获取购物车对象的集合
-		List<Cart> cartList = (List<Cart>) session.getAttribute("cartList");
-		//购物车中所有的商品的对象集合
-		Map<Product, Integer> cartMap = new HashMap<Product, Integer>();
-		//遍历购物车对象
-		for(Cart cart : cartList) {
-			//根据商品id查询商品的信息
-			Product product = dao.findProductByPid(cart.getPid());
-			//将查询到的商品存放到map集合
-			cartMap.put(product, cart.getCount());
-		}
-		request.setAttribute("cartMap", cartMap);
-		request.getRequestDispatcher("cart.jsp").forward(request, response);;
+		Cart cart = (Cart) session.getAttribute("cart");
+		//删除购物车项
+		cart.getCart().remove(pid);
+		response.sendRedirect("cart.jsp");
+		
+	}
+
+
+	private void changeNumber(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String pid = request.getParameter("pid");
+		String number = request.getParameter("number");
+		
+		HttpSession session = request.getSession();
+		Cart cart = (Cart) session.getAttribute("cart");
+		
+		//根据pid取出对应的项
+		CartItem item = cart.getCart().get(pid);
+		item.setNumber(Integer.parseInt(number));
+		
+		//将修改的购物车项存放到购物车中
+		cart.getCart().put(pid, item);
+		
+		//将cart存放到session中
+		session.setAttribute("cart", cart);
+		
+		response.sendRedirect("cart.jsp");
 		return;
 	}
+
+
+	public void addCart(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String pid = request.getParameter("pid");
+		String count = request.getParameter("count");
+		
+		//根据id获取商品
+		Product product = dao.findProductByPid(pid);
+		//构造购物车项
+		CartItem item = new CartItem();
+		item.setProduct(product);
+		item.setNumber(Integer.parseInt(count));
+		//从session获取购物车对象
+		Cart cart = (Cart) session.getAttribute("cart");
+		if (cart == null) {
+			cart = new Cart();
+		}
+		
+		//将购物车项添加到购物车中
+		
+		cart.add(item);
+		
+		//将购物车放到session中
+		
+		session.setAttribute("cart", cart);
+		
+		//跳转到购物车页面
+		response.sendRedirect("cart.jsp");
+		return;
+	}
+	
 
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
